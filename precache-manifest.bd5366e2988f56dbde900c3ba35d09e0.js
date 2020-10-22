acd09a98532537847cfb8e69864a933234e017c4
Gwen Gilet <gwen@withjuno.com>
2020-10-22 16:41:54 +0100

    Updates

---
+++ precache-manifest.bd5366e2988f56dbde900c3ba35d09e0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "486c5ed3eae25c22313ef7df325c7c92",
+    "url": "./index.html"
+  },
+  {
+    "revision": "f6a0cbbfa1a57943562b",
+    "url": "./static/css/main.42cc8b22.chunk.css"
+  },
+  {
+    "revision": "2e9005de78c2a3d81632",
+    "url": "./static/js/2.43303905.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "./static/js/2.43303905.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f6a0cbbfa1a57943562b",
+    "url": "./static/js/main.8624abc4.chunk.js"
+  },
+  {
+    "revision": "b2293ab3a83ac15875b8",
+    "url": "./static/js/runtime-main.66130543.js"
+  }
+]);
